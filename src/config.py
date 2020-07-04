1e42fad098741f52ccdb7885d27ddef3e6aabe37
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-07-04 12:33:45 +0500

    ...

--- src/config.py
+++ src/config.py
@@ -2,5 +2,7 @@ import os
 
 
 class Config:
-    DB_CONNECTION = os.getenv('DB_CONNECTION', 'example.db')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'secrets').encode()
+    DB_CONNECTION = os.getenv('DB_CONNECTION', 'db.sqlite')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret').encode()
+    JSON_SORT_KEYS = False
+
